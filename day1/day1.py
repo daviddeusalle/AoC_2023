f492fddbca9a7d3e4aba0e976698203c892fc0f0
daviddeusalle <daviddeucastells@gmail.com>
2024-07-21 18:27:02 +0200

    to push

---
+++ day1/day1.py
@@ -0,0 +1,24 @@
+
+def read(filename):
+    with open(filename, 'r') as file:
+        return file.readlines()
+
+def one_star(lines):
+    count = 0
+    for line in lines: 
+        start = end = '0'
+        for c in line:
+            if c.isdigit():
+                if start == '0':
+                    start = c
+                end = c
+       
+        count += int(start + end)
+    return count 
+
+lines = read('day1/input.txt')
+print(one_star(lines))
+
+
+
+            
